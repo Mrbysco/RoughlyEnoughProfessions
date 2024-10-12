e00b8b8b06a5b473f3b57a84f87923181d20af9c
Mrbysco <Mrbysco@users.noreply.github.com>
2024-10-12 17:09:15 +0200

    Fix crashing due to providing a server side villager to a render function

--- common/src/main/java/com/mrbysco/roughlyenoughprofessions/VillagerCache.java
+++ common/src/main/java/com/mrbysco/roughlyenoughprofessions/VillagerCache.java
@@ -2,14 +2,13 @@ package com.mrbysco.roughlyenoughprofessions;
 
 import com.mrbysco.roughlyenoughprofessions.platform.Services;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.npc.VillagerProfession;
-import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
 import java.util.function.Function;
 
 public class VillagerCache {
@@ -21,7 +20,7 @@ public class VillagerCache {
 			CompoundTag nbt = new CompoundTag();
 			nbt.putString("id", Services.PLATFORM.getVillagerID());
 			Minecraft mc = Minecraft.getInstance();
-			Level level = mc.hasSingleplayerServer() && mc.getSingleplayerServer() != null ? mc.getSingleplayerServer().getAllLevels().iterator().next() : mc.level;
+			ClientLevel level = Minecraft.getInstance().level;
 			if (level != null) {
 				Villager villager = (Villager) EntityType.loadEntityRecursive(nbt, level, Function.identity());
 				if (villager != null) {
